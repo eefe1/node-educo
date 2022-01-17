6a77e2ff07e4b5c95bcf946677391c0abee03dcc
ezgi <esgiefe@gmail.com>
2022-01-17 09:53:29 +0200

    models,routes,middlewares added

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 const express = require("express");
-
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser')
+const adminRoutes = require("./routes/adminRoutes");
+const blogRoutes = require("./routes/blogRoutes");
+const authRoutes = require('./routes/authRoutes');
+const {requireAuth, checkUser} = require('./middlewares/authMidleware')
 const app = express();
 
+const dbURL =
+  "mongodb+srv://ezgi:206123Es.@nodeserver.xdrun.mongodb.net/NodeServer?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("connected db"))
+  .catch((err) => console.log(err));
+
+app.set("view engine", "ejs");
+
 app.listen(100);
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+
+app.use(cookieParser())
+app.get('*', checkUser)
+app.get("/", (req, res) => {
+  res.redirect("/blog");
+});
+
+app.use('/',authRoutes);
+app.use('/admin',requireAuth,adminRoutes);
+app.use('/blog', blogRoutes);
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About Educo" });
+});
+
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "Page Not Found" });
+});
